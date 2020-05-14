bb883f98c5f0f5d3c44f22affc7200ceb1f132d0
cwJohnPark <pierrotcw@gmail.com>
2020-05-15 06:43:29 +0900

    Init repo

---
+++ src/chapter1/plot/PlotApiTest.java
@@ -0,0 +1,51 @@
+package chapter1.plot;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+
+public class PlotApiTest {
+    public static void main(String[] args) {
+        //functionValues(100);
+        //arrayOfRandomValues(50);
+        sortedArrayOfRandomValues(50);
+    }
+    public static void functionValues(int N) {
+        StdDraw.setXscale(0, N);
+        StdDraw.setYscale(0, N*N);
+        StdDraw.setPenRadius(.01);
+        for (int i = 1; i  <= N; i++) {
+            StdDraw.point(i, i);
+            StdDraw.point(i, i * i);
+            StdDraw.point(i, i * Math.log(i));
+        }
+    }
+    public static void arrayOfRandomValues(int N) {
+        double[] a = new double[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.random();
+        }
+        for (int i = 0; i < N; i++) {
+            double x = 1.0 * 1 / N;
+            double y = a[i] * 2.0;
+            double rw = 0.5 / N;
+            double rh = a[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+    public static void sortedArrayOfRandomValues(int N) {
+        double[] a = new double[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.random();
+        }
+        Arrays.sort(a);
+        for (int i = 0; i < N; i++) {
+            double x = 1.0 * i/N;
+            double y = a[i] / 2.0;
+            double rw = 0.5 / N;
+            double rh = a[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+}
---
+++ src/test/Hello.java
@@ -0,0 +1,11 @@
+package test;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Hello {
+    public static void main(String[] args) {
+        StdOut.print("print test");
+    }
+
+}
